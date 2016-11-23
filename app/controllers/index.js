6fb6044e2c45ecaafcc893e59a8c98bc149d63a1
Jen <jtrudell@gmail.com>
2016-11-23 15:47:58 -0600

    set up to pull from Wikipedia API

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,9 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+
   actions: {
     removeLie() {
       this.set('model.lie.submitted', false);
+      Ember.$.getJSON(
+        "https://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&exintro=&titles=Lie",
+        function(res) {
+          console.log(res);
+        }
+      );
     },
   }
 });
