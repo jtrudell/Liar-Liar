4e51837b3778600387529b9e16973d058b747a85
Jen <jtrudell@gmail.com>
2016-12-18 12:26:17 -0600

    copy and style changes

--- app/routes/application.js
+++ app/routes/application.js
@@ -4,7 +4,8 @@ export default Ember.Route.extend({
   model() {
     const date = new Date();
     const headings = {
-      title: "So, you think you are lying?",
+      title: "You think you are a liar?",
+      subtitle: "If you've made it to this page, you probably aren't lying. But let's do this anyway.",
       year: date.getFullYear(),
     };
     return headings;
--- app/routes/index.js
+++ app/routes/index.js
@@ -4,7 +4,6 @@ export default Ember.Route.extend({
 
   model() {
     return {
-      subtitle: "If you've made it to this page, you probably aren't lying. But let's do this anyway.",
       lie: {
         submitted: null,
         description: null,
